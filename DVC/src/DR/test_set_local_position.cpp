652b13af64f8a68ee23f01fa9eee6f37138b1d01
printfKrzysztof <krzysztofbortnowski@outlook.com>
2022-10-04 15:50:52 +0200

    JUST BEFORE DELETE ALL

--- DVC/src/DR/test_set_local_position.cpp
+++ DVC/src/DR/test_set_local_position.cpp
@@ -6,37 +6,34 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <nav_msgs/Odometry.h>
 
-
-
 sensor_msgs::NavSatFix global_position;
 nav_msgs::Odometry local_position;
 
-void global_pos_cb(const sensor_msgs::NavSatFix::ConstPtr& msg){
+void global_pos_cb(const sensor_msgs::NavSatFix::ConstPtr &msg)
+{
     global_position = *msg;
-    //ROS_INFO("get global pose");
+    // ROS_INFO("get global pose");
 }
 
-void local_pos_cb(const nav_msgs::Odometry::ConstPtr& msg){
+void local_pos_cb(const nav_msgs::Odometry::ConstPtr &msg)
+{
     local_position = *msg;
-    //ROS_INFO("get global pose");
+    // ROS_INFO("get global pose");
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "test_DR");
     ros::NodeHandle test_DR_square("~");
     std::string rosNamespace = test_DR_square.getNamespace();
     ros::Publisher set_local_pos_pub = test_DR_square.advertise<geometry_msgs::Point>(
-            "/drone_ridder/set_local_position", 1);
+        "/drone_ridder/set_local_position", 1);
     ros::Publisher set_heading_pub = test_DR_square.advertise<std_msgs::Float64>("/drone_ridder/set_heading", 1);
-    ros::Publisher set_offset_pub = test_DR_square.advertise<geometry_msgs::Point>("/drone_ridder/set_position_offset",
-                                                                                   1);
+    ros::Publisher set_offset_pub = test_DR_square.advertise<geometry_msgs::Point>("/drone_ridder/set_position_offset", 1);
     ros::Publisher set_mode_pub = test_DR_square.advertise<std_msgs::String>("/drone_ridder/set_mode", 1);
-    ros::Publisher set_global_pos_pub = test_DR_square.advertise<geographic_msgs::GeoPoseStamped>(
-            "/drone_ridder/set_global_position", 1);
+    ros::Publisher set_global_pos_pub = test_DR_square.advertise<geographic_msgs::GeoPoseStamped>("/drone_ridder/set_global_position", 1);
     ros::Subscriber global_pose_sub = test_DR_square.subscribe("/mavros/global_position/global", 1, global_pos_cb);
     ros::Subscriber local_pose_sub = test_DR_square.subscribe("/mavros/global_position/local", 1, local_pos_cb);
-
-
 
     ros::Rate rate(1);
     geometry_msgs::Point waypoint;
@@ -47,15 +44,14 @@ int main(int argc, char** argv) {
     float timeToSleep = 6;
 
     ROS_INFO("Get ready");
-    //ROS_INFO("Test of set_mode");
-    //flightMode.data = "takeoff"
+    // ROS_INFO("Test of set_mode");
+    // flightMode.data = "takeoff"
 
     ros::spinOnce();
     ros::Duration(timeToSleep).sleep();
     ros::spinOnce();
     ros::spinOnce();
     ros::spinOnce();
-
 
     ROS_INFO("Test of set_local_position drone will fly 5m square");
     waypoint.x = 0;
